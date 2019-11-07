480990898a0bcbacdd5e084465c6dab98e51ad1b
Chieko Kanematsu <c.kanematsu2018@gmail.com>
2019-11-07 13:10:35 +0900

    Week1 課題C-4できたよ __init__関数とそれ以外の関数の使い方 CSV形式とはカンマで区切ること リストとは違う。

--- customer.py
+++ customer.py
@@ -6,20 +6,6 @@ ken.full_name()  # "Ken Tanaka" という値を返す
 tom = Customer(first_name="Tom", family_name="Ford")
 tom.full_name()  # "Tom Ford" という値を返す
 """
-
-
-class Customer:
-    def __init__(self, first_name, family_name):
-        self.first_name = first_name
-        self.family_name = family_name
-
-    def full_name(self):
-        print(f'{self.first_name} {self.family_name}')
-
-
-ken = Customer(first_name="Ken", family_name="Tanaka")
-ken.full_name()  # "Ken Tanaka" という値を返す
-
 # C-2. 年齢という概念の追加
 """
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
@@ -31,20 +17,6 @@ tom.age # 57 という値を返す
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 ieyasu.age # 73 という値を返す
 """
-
-
-class Customer:
-    def __init__(self, family_name, first_name, age):
-        self.first_name = first_name
-        self.family_name = family_name
-        self.age = age
-
-
-ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-print(ken.age)  # 15 という値を返す
-
-ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-print(ieyasu.age)  # 73 という値を返す
 
 # C-3. 年齢に応じた適切な入場料(entry_fee)を計算できる
 """
@@ -63,12 +35,28 @@ ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 ieyasu.entry_fee() # 1200 という値を返す
 """
 
+# C-4. 単一の顧客情報をCSV形式で取得できる
+"""
+c
+
+tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+ken.info_csv()  # "Tom Ford,57,1500" という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
+"""
+
 
 class Customer:
     def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
         self.age = age
+
+    def full_name(self):
+        self.full_name = f'{self.first_name}{self.family_name}'
+        return self.full_name
+
 
     def entry_fee(self):
         if self.age < 20:
@@ -78,9 +66,13 @@ class Customer:
         if 65 < self.age:
             return 1200
 
+    def info_csv(self):
+        list = [self.full_name(), str(self.age), str(self.entry_fee())]
+        print(list)
+
+        result = ','.join(list)
+        print(result)
+
 
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-print(ken.entry_fee())  # 1000 という値を返す
-
-ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-ken.entry_fee()  # 1000 という値を返す
+ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
