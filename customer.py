f6439b5bcded21a6a12c96736d58cbd53244d969
Chieko Kanematsu <c.kanematsu2018@gmail.com>
2019-11-07 11:41:33 +0900

    Week1 課題C-2 できたよ     年齢という概念の追加

--- customer.py
+++ customer.py
@@ -19,3 +19,29 @@ class Customer:
 
 ken = Customer(first_name="Ken", family_name="Tanaka")
 ken.full_name()  # "Ken Tanaka" という値を返す
+
+# C-2. 年齢という概念の追加
+"""
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.age  # 15 という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+tom.age # 57 という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.age # 73 という値を返す
+"""
+
+
+class Customer:
+    def __init__(self, family_name, first_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+print(ken.age)  # 15 という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+print(ieyasu.age)  # 73 という値を返す
